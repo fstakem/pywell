9616fc74468453c61fade75bd1cf8a02a3c5ca10
Fredrick Stakem <fstakem@gmail.com>
2018-03-10 00:24:03 -0500

    Adjust time after load

--- scripts/gen_data/load_data.py
+++ scripts/gen_data/load_data.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+from datetime import datetime, timedelta
 
 import pandas as pd
 
@@ -16,8 +17,18 @@ def load_data(path):
     df.index.name = 'Time'
     df.rename(columns={1: 'Values'}, inplace=True)
 
+    return df
+
+def adjust_time(start_time, df):
+    time_interval = df.index[1] - df.index[0]
+    time_interval = timedelta(seconds=time_interval.total_seconds())
+    new_time = [start_time + i * time_interval for i in range(len(df))]
+    df.index = pd.DatetimeIndex(new_time)
+
 def run():
     df = load_data(data_path)
+    current_time = datetime.utcnow()
+    adjust_time(current_time, df)
 
 if __name__ == '__main__':
     run()
